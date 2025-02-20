2a9ce284a72f70b7d197f3b309be64ad4ac7fd5a
markoplk <markopylypyuk@gmail.com>
2025-02-20 15:46:47 +0100

    segundo commit

--- vuln.py
+++ vuln.py
@@ -1,11 +1,10 @@
 import sqlite3
 
-API_KEY = "sk-1234567890abcdef"  # ¡Clave expuesta!
+API_KEY = "AWS-1234567890abcdef"  
 
 user_input = input("Introduce tu nombre: ")
 query = "SELECT * FROM users WHERE name = '" + user_input + "'"
 
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
-cursor.execute(query)  # 🔴 ¡Vulnerabilidad detectada por Semgrep!
-
+cursor.execute(query)
